6382dbc0c34c5a227a1c791bd60f87de8bf4c69d
Rojoin <nachoarrastua@gmail.com>
2023-04-12 01:29:14 -0300

    Add CameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Target")]
+    public Transform target;
+    [Header("Offset")]
+    public Vector3 offset = Vector3.zero;
+    [Header("Limits")]
+    public Vector2 limits = new Vector2(5, 3);
+
+    [Range(0, 1)]
+    public float smoothTime;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            transform.localPosition = offset;
+        }
+
+        FollowTarget(target);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 localPos = transform.localPosition;
+        transform.localPosition = new Vector3(Mathf.Clamp(localPos.x, -limits.x, limits.x), Mathf.Clamp(localPos.y, -limits.y, limits.y), localPos.z);
+    }
+
+    public void FollowTarget(Transform target)
+    {
+        Vector3 localPos = transform.localPosition;
+        Vector3 targetLocalPos = target.transform.localPosition;
+        transform.localPosition = Vector3.SmoothDamp(localPos, new Vector3(targetLocalPos.x + offset.x, targetLocalPos.y + offset.y, localPos.z), ref velocity, smoothTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine( target.parent.transform.position +new Vector3(-limits.x, -limits.y, 0),target.parent.transform.position+ new Vector3(limits.x, -limits.y, 0));
+        Gizmos.DrawLine( target.parent.transform.position +new Vector3(-limits.x, limits.y,  0),target.parent.transform.position+ new Vector3(limits.x, limits.y, 0));
+        Gizmos.DrawLine( target.parent.transform.position +new Vector3(-limits.x, -limits.y, 0),target.parent.transform.position+ new Vector3(-limits.x, limits.y, 0));
+        Gizmos.DrawLine( target.parent.transform.position +new Vector3(limits.x, -limits.y,  0), target.parent.transform.position + new Vector3(limits.x, limits.y,0));
+    }
+
+}
