7013d13b1d0030d74708f673fa4e3fd0c553d6d3
Rojoin <nachoarrastua@gmail.com>
2023-04-17 10:40:52 -0300

    Create PlayerShooting Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,9 @@ public class PlayerMovement : MonoBehaviour
     [Header("GameObjects")]
     [SerializeField]
     Transform playerModel;
-    [SerializeField]
-    Transform rayPosition;
+
     [SerializeField]
     private Transform aimTarget;
-
-    [SerializeField] private ParticleSystem prefire;
-    [SerializeField] private ParticleSystem fireLaser;
-    public Bullet bullet;
     [SerializeField]
     Cinemachine.CinemachineDollyCart dolly;
     [Header("Values")]
@@ -26,23 +21,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movevementValue;
     [SerializeField]
     float leanLimit;
-    public int raycastDistance;
-    private bool isFiringRay;
-    private bool isPressingButton;
-    private float currentBeanTimer;
-    [SerializeField]
-    private float specialBeanTimer = 1.2f;
-    private float specialBeanActiveTime = 1.2f;
-    [SerializeField]
-    private float minHoldShootTimer = 0.2f;
-    private float currentHoldShootTimer;
-    [SerializeField]
-    private float minShootTimer = 0.05f;
-    private float currentSingleShootTimer;
 
-    private bool auxCoroutine = false;
-    private bool singleBulletShoot;
-    private bool hasPreFireBeenDone;
 
     void Start()
     {
@@ -54,59 +33,9 @@ public class PlayerMovement : MonoBehaviour
     {
 
         Movement();
-        AttackLogic();
     }
 
-    private void AttackLogic()
-    {
-        currentHoldShootTimer += Time.deltaTime;
-        currentSingleShootTimer += Time.deltaTime;
-        if (isPressingButton)
-        {
-            currentBeanTimer += Time.deltaTime;
-            if (currentSingleShootTimer > minShootTimer && !singleBulletShoot)
-            {
-                singleBulletShoot = true;
-                currentSingleShootTimer = 0.0f;
-                ShootBullet();
-            }
-            else if (currentHoldShootTimer > minHoldShootTimer && singleBulletShoot)
-            {
-                ShootBullet();
-                currentHoldShootTimer -= minHoldShootTimer;
-            }
 
-            if (currentBeanTimer > specialBeanTimer )
-            {
-                prefire.Play();
-                
-            }
-            else
-            {
-                prefire.Stop();
-            }
-
-        }
-        else
-        {
-            if (currentBeanTimer > specialBeanTimer)
-            {
-                Debug.Log("Disparo");
-                ShootRay();
-            }
-            singleBulletShoot = false;
-
-            currentBeanTimer = 0.0f;
-            currentHoldShootTimer = minHoldShootTimer;
-            hasPreFireBeenDone = false;
-            prefire.Stop();
-        }
-    }
-
-    public void ShootCoroutine()
-    {
-        StartCoroutine(continuosRay());
-    }
 
     private void Movement()
     {
@@ -120,78 +49,6 @@ public class PlayerMovement : MonoBehaviour
     {
 
         movevementValue = ctx.ReadValue<Vector2>();
-
-
-    }
-
-    public void OnFire(InputAction.CallbackContext ctx)
-    {
-        //  ShootBullet();
-        //DebugManager.Instance.Log(this.tag, "Apretaste el gatillo. Fire!!");
-        if (ctx.performed)
-        {
-            isPressingButton = true;
-
-        }
-        else if (ctx.canceled)
-        {
-            isPressingButton = false;
-        }
-    }
-    public void OnFire2(InputAction.CallbackContext ctx)
-    {
-
-        ShootRay();
-
-    }
-
-
-    public void ShootRay()
-    {
-        fireLaser.Play();
-        if (CheckLaserHitBox(out var hit) && hit.collider.CompareTag("Enemy") && hit.collider.GetComponent<EnemyController>().isActive)
-        {
-            hit.collider.GetComponent<EnemyController>().CurrentHealth -= hit.collider.GetComponent<EnemyController>().CurrentHealth;
-            isFiringRay = true;
-
-            Debug.Log("RayoLaser");
-        }
-        else
-        {
-            isFiringRay = false;
-        }
-        // DebugManager.Instance.Log(tag, isFiringRay.ToString());
-
-    }
-
-    IEnumerator continuosRay()
-    {
-        yield return new WaitForEndOfFrame();
-        auxCoroutine = true;
-        var currentTimer = 0.0f;
-        while (currentTimer < specialBeanActiveTime)
-        {
-            currentTimer += Time.fixedDeltaTime;
-
-            ShootRay();
-        }
-        yield break;
-        auxCoroutine = false;
-    }
-    private bool CheckLaserHitBox(out RaycastHit hit)
-    {
-        return Physics.Raycast(rayPosition.position, rayPosition.forward, out hit, raycastDistance) ||
-               Physics.Raycast(rayPosition.position + Vector3.up / 2, rayPosition.forward, out hit, raycastDistance) ||
-               Physics.Raycast(rayPosition.position + Vector3.down / 2, rayPosition.forward, out hit, raycastDistance) ||
-               Physics.Raycast(rayPosition.position + Vector3.right / 2, rayPosition.forward, out hit, raycastDistance) ||
-               Physics.Raycast(rayPosition.position + Vector3.left / 2, rayPosition.forward, out hit, raycastDistance);
-    }
-
-    private void ShootBullet()
-    {
-        bullet.SetStartPosition(transform);
-        bullet.SetActiveState(true);
-        bullet.ResetBulletTimer();
     }
 
     private void LocalMove(float x, float y)
@@ -222,17 +79,6 @@ public class PlayerMovement : MonoBehaviour
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(aimTarget.position, .5f);
         Gizmos.DrawSphere(aimTarget.position, .15f);
-
-        Gizmos.color = Color.red;
-        if (isFiringRay)
-        {
-            Gizmos.color = Color.green;
-        }
-        Gizmos.DrawRay(rayPosition.position, rayPosition.forward * raycastDistance);
-        Gizmos.DrawRay(rayPosition.position + Vector3.up / 2, rayPosition.forward * raycastDistance);
-        Gizmos.DrawRay(rayPosition.position + Vector3.down / 2, rayPosition.forward * raycastDistance);
-        Gizmos.DrawRay(rayPosition.position + Vector3.right / 2, rayPosition.forward * raycastDistance);
-        Gizmos.DrawRay(rayPosition.position + Vector3.left / 2, rayPosition.forward * raycastDistance);
     }
 
 }
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+
+namespace Assets.Scripts
+{
+    public class PlayerShooting : MonoBehaviour
+    {
+        [SerializeField]
+        Transform rayPosition;
+        [SerializeField]
+        private Bullet bullet;
+        [SerializeField]
+        private ParticleSystem prefire;
+        [SerializeField]
+        private ParticleSystem fireLaser;
+        public int raycastDistance;
+        private bool isFiringRay;
+        private bool isPressingButton;
+        private float currentBeanTimer;
+        [SerializeField]
+        private float specialBeanTimer = 1.2f;
+        [SerializeField]
+        private float specialBeanActiveTime = 1.2f;
+        [SerializeField]
+        private float minHoldShootTimer = 0.2f;
+        private float currentHoldShootTimer;
+        [SerializeField]
+        private float minShootTimer = 0.05f;
+        private float currentSingleShootTimer;
+
+        private bool auxCoroutine = false;
+        private bool singleBulletShoot;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+        private void Update()
+        {
+            AttackLogic();
+        }
+        private void AttackLogic()
+        {
+            currentHoldShootTimer += Time.deltaTime;
+            currentSingleShootTimer += Time.deltaTime;
+            if (isPressingButton)
+            {
+                currentBeanTimer += Time.deltaTime;
+                if (currentSingleShootTimer > minShootTimer && !singleBulletShoot)
+                {
+                    singleBulletShoot = true;
+                    currentSingleShootTimer = 0.0f;
+                    ShootBullet();
+                }
+                else if (currentHoldShootTimer > minHoldShootTimer && singleBulletShoot)
+                {
+                    ShootBullet();
+                    currentHoldShootTimer -= minHoldShootTimer;
+                }
+
+                if (currentBeanTimer > specialBeanTimer)
+                {
+                    prefire.Play();
+
+                }
+                else
+                {
+                    prefire.Stop();
+                }
+
+            }
+            else
+            {
+                if (currentBeanTimer > specialBeanTimer)
+                {
+                    Debug.Log("Disparo");
+                    //StartCorroutine(continuosRay());
+                    ShootRay();
+                }
+                singleBulletShoot = false;
+
+                currentBeanTimer = 0.0f;
+                currentHoldShootTimer = minHoldShootTimer;
+                prefire.Stop();
+            }
+        }
+
+        public void OnFire(InputAction.CallbackContext ctx)
+        {
+            //  ShootBullet();
+            //DebugManager.Instance.Log(this.tag, "Apretaste el gatillo. Fire!!");
+            if (ctx.performed)
+            {
+                isPressingButton = true;
+
+            }
+            else if (ctx.canceled)
+            {
+                isPressingButton = false;
+            }
+        }
+        private void ShootBullet()
+        {
+            //var newBullet = Instantiate(bullet);
+            bullet.SetStartPosition(transform);
+            bullet.SetActiveState(true);
+            bullet.ResetBulletTimer();
+        }
+        public void ShootRay()
+        {
+            fireLaser.Play();
+            if (CheckLaserHitBox(out var hit) && hit.collider.CompareTag("Enemy") && hit.collider.GetComponent<EnemyController>().isActive)
+            {
+                hit.collider.GetComponent<EnemyController>().CurrentHealth -= hit.collider.GetComponent<EnemyController>().CurrentHealth;
+                isFiringRay = true;
+
+                Debug.Log("RayoLaser");
+            }
+            else
+            {
+                isFiringRay = false;
+            }
+            // DebugManager.Instance.Log(tag, isFiringRay.ToString());
+
+        }
+        private bool CheckLaserHitBox(out RaycastHit hit)
+        {
+            return Physics.Raycast(rayPosition.position, rayPosition.forward, out hit, raycastDistance) ||
+                   Physics.Raycast(rayPosition.position + Vector3.up / 2, rayPosition.forward, out hit, raycastDistance) ||
+                   Physics.Raycast(rayPosition.position + Vector3.down / 2, rayPosition.forward, out hit, raycastDistance) ||
+                   Physics.Raycast(rayPosition.position + Vector3.right / 2, rayPosition.forward, out hit, raycastDistance) ||
+                   Physics.Raycast(rayPosition.position + Vector3.left / 2, rayPosition.forward, out hit, raycastDistance);
+        }
+    }
+}
